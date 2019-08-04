476ca07340463d2d4542ae788864bd48b6158eaa
lyf <liyuanfeng008@gmail.com>
2019-08-04 08:34:42 +0800

    access /api/users

--- www/test.py
+++ www/test.py
@@ -3,6 +3,7 @@ import orm
 import random
 from models import User, Blog, Comment
 
+
 @asyncio.coroutine
 def test(loop):
     yield from orm.create_pool(loop, user='www', password='password', database='awesome')
@@ -14,7 +15,8 @@ def test(loop):
 
 # 要运行协程，需要使用事件循环
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(test(loop))
-    print('Test finished.')
-    loop.close()
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(test(loop))
+    # print('Test finished.')
+    # loop.close()
+    pass
